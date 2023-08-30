3fc19c3083e9d8ccb6ca7c4e5881a2393d39e75c
FranciscoAndreoli <franandreoli7@gmail.com>
2023-08-29 23:23:41 -0300

    Add project files.

---
+++ alkemyumsa/Entities/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace alkemyumsa.Entities
+{
+    public class Login
+    {
+        public Login() {
+            this.Id = 1;
+            this.Name = "Pepe";
+            this.Password = "1234";
+        }
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public string Password { get; set; }
+
+    }
+}
