31f5a75e90ca45ba2d777b0da0886c755e918d1d
FranciscoAndreoli <franandreoli7@gmail.com>
2023-09-04 23:40:00 -0300

    Added controllers, entities, and DTO´s

--- alkemyumsa/Controllers/LoginController.cs
+++ alkemyumsa/Controllers/LoginController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using alkemyumsa.DTOs;
 using alkemyumsa.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace alkemyumsa.Controllers
 {
@@ -9,24 +12,133 @@ namespace alkemyumsa.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        // Simular base de datos en memoria
+        private static List<Login> login = new()
+        {
+            new Login { Id = 1, Name = "Juan", Password = "123"},
+            new Login { Id = 2, Name = "Maria", Password = "1234"}
+        };
 
-        [HttpGet]
-        [Route("api/getUsuario")]
+
+        /// <summary>
+        /// Devuelve todos los usuarios
+        /// </summary>
+        /// <returns> todos los usuarios</returns>
+        [HttpGet("getUsuarios")]
+        public ActionResult<LoginDto> GetUsuarios()
+        {
+            var loginDto = login.Select(e => new LoginDto
+            {
+                Id = e.Id,
+                Name = e.Name
+            }).ToList();
+            return Ok(loginDto);
+        }
+       
+
+        /// <summary>
+        /// Devuelve un usuario, segun ID
+        /// </summary>
+        /// <param name="id"> Entero</param>
+        /// <returns>Un usuario</returns>
+        [HttpGet("getUsuario/{id}")]
         public ActionResult<LoginDto> GetUsuario(int id)
         {
-            Login usuario = new Login();
-            if (id == 1)
+            try
             {
-                LoginDto usuarioDTO = new LoginDto()
+                var usuario = login.FirstOrDefault(e => e.Id == id);
+                if (usuario == null)
+                {
+                    return NotFound();
+                }
+                var LoginDto = new LoginDto
                 {
                     Id = usuario.Id,
-                    Name = usuario.Name
+                    Name = usuario.Name,
+
                 };
-            return Ok(usuarioDTO);
+
+                return Ok(LoginDto);
             }
-            return NotFound("User not found");
+            catch (Exception ex)
+            {
+                return BadRequest($"Error del sistema: {ex.Message}");
+            }
+        }
+
+
+
+        /// <summary>
+        /// Crea un usuario
+        /// </summary>
+        /// <param name="loginDto">DTO de Login</param>
+        /// <returns>Devuelve URL del recurso creado.</returns>
+        [HttpPost("postUsuario")]
+        public ActionResult<LoginDto> CreateUsuario(LoginDto loginDto)
+        {
+            try
+            {
+                var usuario = new Login
+                {
+                    Id = login.Max(e => e.Id) + 1, // Generating new Id
+                    Name = loginDto.Name
+                };
+
+                login.Add(usuario);
+
+                loginDto.Id = usuario.Id;
+
+                return CreatedAtAction(nameof(GetUsuario), new { id = usuario.Id }, loginDto); //Es una buena practica que POST luego de crear un recurso debe devolver un estado 201 y proporcionar una URL al recurso creado. Eso es lo que sucede en este return
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error del sistema: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Actualiza o reemplaza un usuario
+        /// </summary>
+        /// <param name="id"> integer </param>
+        /// <param name="loginDto">DTO de Login</param>
+        /// <returns>Retorna not found si no se encontró usuario. Si se encontró, no retorna contenido</returns>
+        [HttpPut("putUsuario/{id}")]
+        public ActionResult<LoginDto> UpdateUsuario(int id, LoginDto loginDto)
+        {
+            var usuario = login.FirstOrDefault(e => e.Id == id); //devolverá el primer elemento que cumpla con la condición. Sino, devuelve null.
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            usuario.Id = loginDto.Id;
+            usuario.Name = loginDto.Name;
+
+            return NoContent();
+        }
+
+
+        /// <summary>
+        /// Borra usuario.
+        /// </summary>
+        /// <param name="id">integer</param>
+        /// <returns>Retorna not found si no se encontró usuario. Si se encontró, no retorna contenido</returns>
+        [HttpDelete("deleteteUsuario/{id}")]
+        public ActionResult<LoginDto> DeleteUsuario(int id)
+        {
+            var usuario = login.FirstOrDefault(e => e.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            login.Remove(usuario);
+
+            return NoContent();
         }
     }
 }
+
+
 
 
---
+++ alkemyumsa/Entities/Servicios.cs
@@ -0,0 +1,11 @@
+﻿namespace alkemyumsa.Entities
+{
+    public class Servicios
+    {
+        public int CodServicio { get; set; }
+        public string? Descr { get; set; }
+        public bool Estado { get; set; }
+        public int ValorHora { get; set; }
+
+    }
+}
